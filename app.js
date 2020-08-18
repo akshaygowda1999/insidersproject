14929a95e48798a7e903a74440f25a31d8c979c7
akshaygowda1999 <46803044+akshaygowda1999@users.noreply.github.com>
2020-08-18 14:48:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,67 @@
-var express = require('express');
-var app=express();
-var bodyParser=require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
-app.get('/',function(req,res){
-    res.sendFile('/Users/mac/desktop/new/index.html',{root: __dirname});
+//major-project-WD 
+//use mongodb database to see the data stored via nodejs 
+// mongoose.connect('mongodb://localhost:27017/interior12'  //line no:19
+//in the above code u have to connect it with a db (In the above url, interior12 is the database)
+//MongoDB, by default, runs on port 27017
+
+
+var express = require("express");
+var app = express();
+var port = 8000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require("mongoose");
+   mongoose.Promise = global.Promise;
+  mongoose.connect("mongodb://localhost:27017/interior12",{ useMongoClient: true });
+
+   var nameSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    contact: String,
+    description: String
+   });
+   
+   
+   var User = mongoose.model("User", nameSchema);
+   app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/contact.html");
+   });
+   app.post("/submit-student-data", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("thanks for your feedback!!!");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
 });
-app.post('/submit-student-data',function(req,res){
-    var name=req.body.firstName+' '+req.body.lastName;
-    res.send(name+'submitted successfully!');
+app.listen(port, () => {
+ console.log("Server listening on port " + port);
 });
-app.delete('/delete-data',function(req,res){
-    res.send('DELETE Request');
-});
-var server=app.listen(5000,function(){
-    console.log('nodeserver is running');
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
